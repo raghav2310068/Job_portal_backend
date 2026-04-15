06771af300a2d22a1653a0747825bbfec293fbf8
Raghav <raghavmittalbsr@gmail.com>
2026-04-15 18:04:52 +0530

    added host

--- job_portal_main/settings.py
+++ job_portal_main/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["job-portal-backend-zo1b.onrender.com"]
 
 
 # Application definition
