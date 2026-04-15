1c5a968cd0891bba3d6e494a1eac73a45d5e1472
Raghav <raghavmittalbsr@gmail.com>
2026-04-15 17:55:28 +0530

    initial commit - job portal backend

--- job_portal_main/settings.py
+++ job_portal_main/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for job_portal_main project.
 
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@2@b)q%yruy-*!9n7v)#@+pyj&u0k2i1m6&=30f+i8m60zav3v"
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
